c0149a77a82239c0ce1a5609b9978dc643e628de
JDMTias <matiasiturbide@gmail.com>
2020-05-08 10:29:40 -0600

    Header done questions already done before as well

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    const headerDiv = document.createElement('div');
+    const spanDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const spanTemp = document.createElement('span')
+
+    headerDiv.appendChild(spanDate);
+    headerDiv.appendChild(headerTitle);
+    headerDiv.appendChild(spanTemp);
+
+    headerDiv.classList.add('header');
+    spanDate.classList.add('date');
+    spanTemp.classList.add('temp');
+
+    spanDate.textContent = 'SMARCH 28, 2019';
+    headerTitle.textContent = 'Lambda Times';
+    spanTemp.textContent = '98';
+
+    return headerDiv
+
+}
+
+const headerContainer = document.querySelector('.header-container');
+
+headerContainer.appendChild(Header());
